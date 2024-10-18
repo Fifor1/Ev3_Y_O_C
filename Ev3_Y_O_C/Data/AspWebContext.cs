4ee9334ae0bbbc98ce8be1c2be8e7979ee137c78
oscar <sogunder@gmail.com>
2024-10-18 01:48:06 -0300

    fix a la base de datos, se crean migraciones correctas

--- Ev3_Y_O_C/Data/AspWebContext.cs
+++ Ev3_Y_O_C/Data/AspWebContext.cs
@@ -20,43 +20,7 @@ namespace Ev3_Y_O_C.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            // Asegura que el número de serie sea único
-            modelBuilder.Entity<Herramienta>()
-                .HasIndex(h => h.NumeroSerie)
-                .IsUnique();
-
-            // Configuración de relaciones
-            modelBuilder.Entity<Herramienta>()
-                .HasOne(h => h.Modelo)
-                .WithMany(m => m.Herramientas)
-                .HasForeignKey(h => h.ModeloId);
-
-            modelBuilder.Entity<Asignacion>()
-                .HasOne(a => a.Herramienta)
-                .WithMany(h => h.Asignaciones)
-                .HasForeignKey(a => a.HerramientaId);
-
-            modelBuilder.Entity<Asignacion>()
-                .HasOne(a => a.Usuario)
-                .WithMany(u => u.Asignaciones)
-                .HasForeignKey(a => a.UsuarioId);
-
-            modelBuilder.Entity<Movimiento>()
-                .HasOne(m => m.Herramienta)
-                .WithMany(h => h.Movimientos)
-                .HasForeignKey(m => m.HerramientaId);
-
-            modelBuilder.Entity<Movimiento>()
-                .HasOne(m => m.Usuario)
-                .WithMany(u => u.Movimientos)
-                .HasForeignKey(m => m.UsuarioId);
-
-            // Relación Marca - Modelo
-            modelBuilder.Entity<ModeloHerramienta>()
-                .HasOne(m => m.Marca)
-                .WithMany(m => m.Modelos)
-                .HasForeignKey(m => m.MarcaId);
+           
         }
     }
 }
--- Ev3_Y_O_C/Migrations/20241018044625_migraciones.Designer.cs
+++ Ev3_Y_O_C/Migrations/20241018044625_migraciones.Designer.cs
@@ -4,6 +4,7 @@ using Ev3_Y_O_C.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ev3_Y_O_C.Migrations
 {
     [DbContext(typeof(AspWebContext))]
-    partial class AspWebContextModelSnapshot : ModelSnapshot
+    [Migration("20241018044625_migraciones")]
+    partial class migraciones
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
