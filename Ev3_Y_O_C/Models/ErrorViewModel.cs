85dd2ad64cc2192f3f40a120ea401a88abebf12b
Cristopher Venegas Araya <cristofer.fifor.fuentes@gmail.com>
2024-10-17 22:39:06 -0300

    Agregar archivos de proyecto.

---
+++ Ev3_Y_O_C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ev3_Y_O_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
