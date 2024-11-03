2c2628ac4541e2637a3d6c281c0c0f9add49341f
Moka <ymunoz23@cftsa.cl>
2024-11-03 00:50:45 -0300

    Creacion de viewmodel de HerrameintaUsuario y del archivo Style.css

---
+++ Ev3_Y_O_C/Models/HerramientaUsuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Ev3_Y_O_C.Models
+{
+    public class HerramientaUsuarioViewModel
+    {
+        public DateTime FechaIngreso { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombreHerramienta { get; set; }
+        public string Estado { get; set; }
+    }
+}
